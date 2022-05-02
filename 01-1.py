d97b3c5c9fa17f13915528f06ab5a955c931b671
Aleexo <alexandre@lixo.tech>
2022-05-02 20:00:39 +0200

    challenge 1

--- 01-1.py
+++ 01-1.py
@@ -1,3 +1,11 @@
 from utils import read_inputs
 
 inputs = read_inputs("inputs/01/1.txt")
+
+inputs = {int(x) for x in inputs}
+for x in inputs:
+    if 2020 - x in inputs:
+        result = x * (2020 - x)
+        break
+
+print(result)
---
+++ 01-2.py
@@ -0,0 +1,12 @@
+from utils import read_inputs
+
+inputs = read_inputs("inputs/01/1.txt")
+
+inputs = [int(x) for x in inputs]
+for i, x in enumerate(inputs):
+    for j, y in enumerate(inputs[i:]):
+        for z in inputs[j:]:
+            if x + y + z == 2020:
+                result = x * y * z
+
+print(result)
