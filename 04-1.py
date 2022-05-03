7b47b9e9590b66e4e7f95abc5fe419fab2dd0aec
Samuel <samuel.stephanscolaire@hotmail.com>
2022-05-03 21:12:39 +0200

    problem 1 day 4

--- 04-1.py
+++ 04-1.py
@@ -1,1 +1,28 @@
-print("marine me pique mon travail")
+from utils import read_inputs
+inputs = read_inputs("inputday4.txt")
+
+
+nbpasseport=inputs.count('')
+print(nbpasseport)
+indexnextpasseport=0
+indexcurrentpasseport=0
+goodpasseport=0
+for i in range(nbpasseport):
+    numberofkey=0
+    currentpasseportkey=[]
+    while inputs[indexnextpasseport]!='':
+        indexnextpasseport+=1
+        
+    idpasseport=[]
+    idpasseport=" ".join([inputs[idx] for idx in range(indexcurrentpasseport,indexnextpasseport)])
+    idpasseportsplitkey=idpasseport.split(" ")
+    
+    for k in range(len(idpasseportsplitkey)):
+        currentpasseportkey.append(idpasseportsplitkey[k].split(":")[0]) 
+    
+    if all([element in currentpasseportkey for element in ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]]):
+        goodpasseport+=1
+    
+    indexnextpasseport=indexnextpasseport+1
+    indexcurrentpasseport=indexnextpasseport
+print(goodpasseport)
